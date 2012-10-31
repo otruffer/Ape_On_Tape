1d89d06d4f17caf4750f1ba386633a84576ea839
Oskar Truffer <oskar.truffer@gmail.com>
2012-10-31 21:26:37 +0100

    added missing files

---
+++ src/server/GsonExclusionStrategy.java
@@ -0,0 +1,32 @@
+package server;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+
+
+public class GsonExclusionStrategy implements ExclusionStrategy {
+    private final Class<?> typeToSkip;
+
+    public GsonExclusionStrategy(Class<?> typeToSkip) {
+      this.typeToSkip = typeToSkip;
+    }
+
+    public boolean shouldSkipClass(Class<?> clazz) {
+      return (clazz == typeToSkip);
+    }
+
+    public boolean shouldSkipField(FieldAttributes f) {
+      return f.getAnnotation(noGson.class) != null;
+    }
+    
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.FIELD})
+    public @interface noGson {
+      // Field tag only annotation
+    }
+}
---
+++ src/server/exceptions/NotAPlayerException.java
@@ -0,0 +1,10 @@
+package server.exceptions;
+
+public class NotAPlayerException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
