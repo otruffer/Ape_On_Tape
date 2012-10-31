7ad72d4844afad73b8b7ed6a129457ec6c5e903c
Oskar Truffer <oskar.truffer@gmail.com>
2012-10-31 15:09:11 +0100

    random starting point

--- src/server/TileMap.java
+++ src/server/TileMap.java
@@ -1,9 +1,12 @@
 package server;
 
+import java.util.Random;
+
 public class TileMap {
 	private Tile[][] tileMap;
 	private float tileHeight = 30;
 	private float tileWidth = 30;
+	//random starting pos atm.
 	private static int[] START_TYLE = {1,1};
 	
 	public TileMap(int[][] mapArray){
@@ -40,6 +43,18 @@ public class TileMap {
 		return tile;
 	}
 	
+	public Tile getRandomWalkableTile(){
+		Tile tile = null;
+		while(tile == null){
+			Random rnd = new Random();
+			int y = rnd.nextInt(this.tileMap.length);
+			int x = rnd.nextInt(this.tileMap[0].length);
+			if(tileMap[y][x].isWalkable())
+				tile = tileMap[y][x];
+		}
+		return tile;
+	}
+	
 	public int getHeight(){
 		return tileMap.length;
 	}
@@ -49,7 +64,9 @@ public class TileMap {
 	}
 
 	public float[] getStartXY() {
-		float[] xy = {START_TYLE[1]*tileHeight, START_TYLE[1]*tileWidth};
+		//float[] xy = {START_TYLE[1]*tileHeight, START_TYLE[1]*tileWidth};
+		Tile randomWalk = this.getRandomWalkableTile();
+		float[] xy = {randomWalk.getY()*tileHeight, randomWalk.getX()*tileWidth};
 		return xy;
 	}
 }
