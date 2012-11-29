3696fcd9f5377647086449030a229025ea548966
Cedric Reichenbach <didu@gmx.ch>
2012-11-29 22:23:49 +0100

    aggressive bot is working now

--- src/server/model/AggroBot.java
+++ src/server/model/AggroBot.java
@@ -4,12 +4,47 @@ import java.util.List;
 
 public class AggroBot extends Bot {
 
+	private static final float MOVE_DIRECTION_MEMORY = 15;
+	private float lastMoveX;
+	private float lastMoveY;
+
 	public AggroBot(int id, float x, float y, String name) {
 		super(id, x, y, name);
+		this.lastMoveX = Float.MAX_VALUE;
+		this.lastMoveY = Float.MAX_VALUE;
 	}
 
 	@Override
 	protected List<Entity> move(Game game, float dX, float dY) {
-		return super.move(game, dX * (Math.abs(dY) + 1), dY + (Math.abs(dX) + 1));
+		float beforeX = this.getX();
+		float beforeY = this.getY();
+
+		List<Entity> result;
+		if (euclideanLength(lastMoveX, lastMoveY) < this.speed / 2)
+			if (abs(lastMoveX) > abs(lastMoveY))
+				result = super.move(game, dX + memoryX(), 0 + memoryY());
+			else
+				result = super.move(game, 0 + memoryX(), dY + memoryY());
+		else
+			result = super.move(game, dX + memoryX(), dY + memoryY());
+
+		float afterX = this.getX();
+		float afterY = this.getY();
+		this.lastMoveX = afterX - beforeX;
+		this.lastMoveY = afterY - beforeY;
+
+		return result;
+	}
+
+	private float memoryX() {
+		return lastMoveX * MOVE_DIRECTION_MEMORY;
+	}
+
+	private float memoryY() {
+		return lastMoveY * MOVE_DIRECTION_MEMORY;
+	}
+
+	private float abs(float f) {
+		return f < 0 ? -f : f;
 	}
 }
